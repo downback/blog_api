d4dae31496ad2068e33b4f6bb908d2878fea3195
downback <daeun.down.back@gmail.com>
2024-01-21 10:59:56 +0100

    DB fetch

--- src/App.js
+++ src/App.js
@@ -1,8 +1,29 @@
-function App() {
-  return (
-    <div className="App">
+import React, { useEffect, useState }  from 'react';
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
 
-    </div>
+function App() {
+  const [ blog , setBlog ] = useState();
+  console.log(blog);
+
+  useEffect(() => {
+    fakeBlog();
+  }, []);
+
+  const fakeBlog = async() => {
+    const response = await fetch("https://api.slingacademy.com/v1/sample-data/blog-posts");
+    //console.log(response);
+    const jsonData = await response.json();
+    //console.log(jsonData);
+    setBlog(jsonData);
+  }
+
+  return (
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
   );
 }
 
