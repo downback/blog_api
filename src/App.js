d1ef8e1bae98e3779fe1c60be9b50c766d9a2031
downback <daeun.down.back@gmail.com>
2024-01-23 19:54:43 +0100

    CHANGE pagenumber method

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import "./index.css"
 function App() {
   const [posts, setPosts] = useState([]);
   const [currentPage, setCurrentPage] = useState(1);
-  const [postsPerPage] = useState(3);
+  const postsPerPage = 3;
 
   useEffect(() => {
     const fetchPosts = async () => {
@@ -30,17 +30,19 @@ function App() {
   const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const totalPages = Math.ceil(posts.length / postsPerPage);
 
   // Change page
-  const paginate = (pageNumber) => setCurrentPage(pageNumber) ;
+  function paginate(pageNumber){
+    setCurrentPage(pageNumber)
+  };
 
   return (
     <div className="w-screen h-full flex flex-col justify-center items-center my-5">
       <Header />
       <Posts posts={currentPosts} />
       <Pagination
-        postsPerPage={postsPerPage}
-        totalPosts={posts.length}
+        totalPages={totalPages}
         paginate={paginate}
       />
     </div>
