c6a086aed2583d1a37c1843a65b5c972173c5b27
downback <daeun.down.back@gmail.com>
2024-01-24 00:22:33 +0100

    ADD blog page style

--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -11,8 +11,13 @@ const Blog = () => {
   return (   
     <div>
       <Header />
-      <h1>This is Blog{id}</h1>
       {/*<h1>{posts.title}</h1>*/}
+      <div className='flex w-screen h-fit justify-center content-center'>
+        <div className="border rounded-sm border-current px-5 py-5 m-5 w-6/12 h-max text-center hover:bg-slate-100">
+          <h1 className='font-semibold text-lg mb-3'>This is Blog Title{id}</h1>
+          <p>Here is the body of Blog{id}</p>
+        </div>
+      </div>
     </div>
   )
 }
