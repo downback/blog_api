f19bfd4be8ef34de5b7950e9ef801a728e142011
downback <daeun.down.back@gmail.com>
2024-01-24 00:18:07 +0100

    costom hook tried but not working

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import axios from "axios";
+import { useEffect, useState } from "react";
 
-const useFetch = () => {
 
-}
+const useFetch = (url) => {
+  const [posts, setPosts] = useState(null);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
-export default useFetch
+  useEffect(() => {
+    setLoading(true);
+    axios
+      .get(url)
+      .them((res) => {
+        setPosts(res.data);
+      })
+      .catch((err) => {
+        setError(err);
+      }).finally(() => {
+        setLoading(false);
+      })
+  }, [url]);
+
+  return {posts, loading, error};
+};
+
+export default useFetch;
--- src/pages/Blog.jsx
+++ src/pages/Blog.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router'
 import Header from '../components/Header'
+//import useFetch from '../hooks/useFetch'
 
 const Blog = () => {
-  const {id} = useParams()
+  const {id} = useParams();
+
+  //const {posts} = useFetch("https://dummyjson.com/posts?limit=30");
 
   return (   
     <div>
       <Header />
       <h1>This is Blog{id}</h1>
+      {/*<h1>{posts.title}</h1>*/}
     </div>
   )
 }
