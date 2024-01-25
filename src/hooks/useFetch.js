9f829d5c4ed09abb52b7d92eefbc8a62be5fadb9
downback <daeun.down.back@gmail.com>
2024-01-25 22:41:35 +0100

    hooks misspelling

--- src/hooks/useFetch.js
+++ src/hooks/useFetch.js
@@ -1,27 +1,27 @@
-import axios from "axios";
-import { useEffect, useState } from "react";
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const useFetch = (url) => {
-  const [posts, setPosts] = useState(null);
-  const [loading, setLoading] = useState(false);
-  const [error, setError] = useState(null);
+  const [posts, setPosts] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
 
   useEffect(() => {
-    setLoading(true);
+    setLoading(true)
     axios
       .get(url)
-      .them((res) => {
-        setPosts(res.data);
+      .then((res) => {
+        setPosts(res.data)
       })
       .catch((err) => {
-        setError(err);
-      }).finally(() => {
-        setLoading(false);
+        setError(err)
       })
-  }, [url]);
+      .finally(() => {
+        setLoading(false)
+      })
+  }, [url])
 
-  return {posts, loading, error};
-};
+  return { posts, loading, error }
+}
 
-export default useFetch;
+export default useFetch
