74f611fc94d75908a769aa548305261eac78ef21
juepark <juepark@artue.io>
2024-02-03 21:29:57 +0900

    remove provider code from index.js

--- src/index.js
+++ src/index.js
@@ -3,13 +3,10 @@ import ReactDOM from 'react-dom/client'
 import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
-import { BlogsProvider } from './context/blogsContext'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 root.render(
   <BrowserRouter>
-    <BlogsProvider>
-      <App />
-    </BlogsProvider>
+    <App />
   </BrowserRouter>
 )
